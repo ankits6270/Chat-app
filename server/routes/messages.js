bc7d1ad400c815098718ad5bcb25dcd39e8a7a71
Ankit Singh <94107791+ankits6270@users.noreply.github.com>
2024-05-14 15:36:50 +0530

    Commit 2

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,5 +1,6 @@
 const { addMessage, getMessages } = require("../controllers/messageController");
 const router = require("express").Router();
+const express = require('express');
 
 router.post("/addmsg/", addMessage);
 router.post("/getmsg/", getMessages);
